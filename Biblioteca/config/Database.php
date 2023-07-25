49da19ec320825279e806ffa84c11f97dc163562
Teresa1965 <132339878+Teresa1965@users.noreply.github.com>
2023-07-25 15:45:12 +0200

    subida fotos

---
+++ Biblioteca/config/Database.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = ""; // Dejar la contraseña en blanco si no hay contraseña
+$dbname = "biblioteca_db";
+// Crear una conexión a la base de datos
+$conn = new mysqli($host, $username, $password, $dbname);
+// Verificar si hay errores en la conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+// Asignar la conexión a la variable $db para poder utilizarla en otros archivos
+$db = $conn;
+?>
