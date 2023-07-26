9f5bc189abaf9eb5fdb8d51b272d6180e316b27b
Teresa1965 <132339878+Teresa1965@users.noreply.github.com>
2023-07-26 13:03:31 +0200

    subida de footer

--- Biblioteca/view/head/footer.php
+++ Biblioteca/view/head/footer.php
@@ -1,5 +1,31 @@
 </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-  </body>
-</html>
+
+
+<!-- 
+    <footer style="background-color: black; color: white; text-align: center; padding: 10px;">
+    <div class="container">
+        <p>Derechos reservados © Tu Biblioteca Virtual 2023.Visita nuestro sitio web.</p>
+    </div>
+</footer>
+</body>
+</html> -->
+
+<style>
+    footer {
+        background-color: black;
+        color: white;
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50px; /* Ajusta la altura según sea necesario */
+    }
+</style>
+
+<footer>
+    <div>
+        <p style="margin: 0;">Derechos reservados © Tu Biblioteca Virtual 2023. Visita nuestro sitio web.</p>
+    </div>
+</footer>
