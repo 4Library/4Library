85a707639f68b7a75ec9d79d60e5c45c6a70dc1c
dkhurlop <denisekhurlopian@gmail.com>
2023-07-26 13:44:44 +0200

    refactorizando main

--- Biblioteca/view/books/create.php
+++ Biblioteca/view/books/create.php
@@ -1,35 +1,39 @@
 <?php
-    require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/head.php");
+require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/head.php");
 ?>
 
-    <form action="store.php" method="POST" autocomplete="off">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Título del libro</label>
+<form action="store.php" method="POST" autocomplete="off">
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Título del libro</label>
 
-            <input type="text" name="title" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" name="title" required class="form-control" id="exampleInputEmail1"
+            aria-describedby="emailHelp">
 
-            <label for="exampleInputEmail1" class="form-label">Autor del libro</label>
+        <label for="exampleInputEmail1" class="form-label">Autor del libro</label>
 
-            <input type="text" name="author" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" name="author" required class="form-control" id="exampleInputEmail1"
+            aria-describedby="emailHelp">
 
-            <label for="exampleInputEmail1" class="form-label">Portada del libro</label>
+        <label for="exampleInputImageURL" class="form-label">URL de la imagen</label>
+        <input type="text" name="image_url" required class="form-control" id="exampleInputImageURL"
+            placeholder="Ingresa la URL de la imagen">
 
-            <input type="" name="image" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 
-            <label for="exampleInputEmail1" class="form-label">Descripción del libro</label>
+        <label for="exampleInputEmail1" class="form-label">Descripción del libro</label>
 
-            <input type="" name="description" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="" name="description" required class="form-control" id="exampleInputEmail1"
+            aria-describedby="emailHelp">
 
-            <label for="exampleInputEmail1" class="form-label">Número del código de barra del libro</label>
+        <label for="exampleInputEmail1" class="form-label">Número del código de barra del libro</label>
 
-            <input type="" name="isbn" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
+        <input type="" name="isbn" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
 
-        <button type="submit" class="btn btn-primary">Guardar</button>
+    <button type="submit" class="btn btn-primary">Guardar</button>
 
-        <a href="index.php" class="btn btn-danger">Cancelar</a>
-    </form>
+    <a href="index.php" class="btn btn-danger">Cancelar</a>
+</form>
 
 <?php
-    require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/footer.php");
+require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/footer.php");
 ?>
