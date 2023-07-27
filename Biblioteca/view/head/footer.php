6cd84075c149f06b9a4819d6ac3761356d9b3d8b
Teresa1965 <132339878+Teresa1965@users.noreply.github.com>
2023-07-27 09:19:37 +0200

    footer finalizado

--- Biblioteca/view/head/footer.php
+++ Biblioteca/view/head/footer.php
@@ -3,14 +3,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
 
-<!-- 
-    <footer style="background-color: black; color: white; text-align: center; padding: 10px;">
-    <div class="container">
-        <p>Derechos reservados © Tu Biblioteca Virtual 2023.Visita nuestro sitio web.</p>
-    </div>
-</footer>
-</body>
-</html> -->
+
 
 <style>
     footer {
