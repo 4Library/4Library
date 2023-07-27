4aa21650115c2cbc04268bc0ba3d67249d826d2a
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-07-27 14:59:38 +0200

    search box arrangements

--- Biblioteca/view/books/search.php
+++ Biblioteca/view/books/search.php
@@ -1,5 +1,5 @@
 <?php
-// Conectarse a la base de datos (reemplaza los valores con los de tu conexión)
+
 $host= "localhost";
 $dbname="library_db";
 $book = "root";
@@ -14,13 +14,13 @@ if (isset($_GET['query'])) {
     $search_query = $_GET['query'];
 
     // Realizar la consulta en la base de datos //
-    $sql = "SELECT * FROM tabla_libros WHERE titulo LIKE '%$search_query%' OR autor LIKE '%$search_query%'";
+    $sql = "SELECT * FROM tabla_books WHERE titulo LIKE '%$search_query%' OR autor LIKE '%$search_query%'";
     $result = $conn->query($sql);
 
     // Mostrar los resultados
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo "<p>" . $row["titulo"] . " - " . $row["autor"] . "</p>";
+            echo "<p>" . $row["title"] . " - " . $row["author"] . "</p>";
         }
     } else {
         echo "No se encontraron resultados.";
