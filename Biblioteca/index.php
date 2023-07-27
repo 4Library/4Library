352961cd032b5376d572aa2e3e0f1e6dbf91b2e6
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-07-27 14:07:17 +0200

    Reconstrucción del index.php en la carpeta view, y el inde.php general, inclusión del logo

--- Biblioteca/index.php
+++ Biblioteca/index.php
@@ -1,51 +1,13 @@
 <?php
-require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/head.php");
-require_once("c://xampp/htdocs/4Library/Biblioteca/controller/BookController.php");
-
-$obj = new booksController();
-$rows = $obj->indexBooks();
+    require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/head.php");
 ?>
 
-<div class="row justify-content-start">
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <a href="/4Library/Biblioteca/view/books/create.php" class="btn btn-primary btn-block">Agregar
-                libro</a>
-        </div>
-    </div>
-    <h2 class="text-center mb-5">¡Bienvenido a tu rincon de lecturas!</h2>
-<div class="container">
+<?php
+    require_once("c://xampp/htdocs/4Library/Biblioteca/view/books/index.php");
+?>
 
-    <?php if (isset($rows) && !empty($rows)): ?>
-        <div class="row">
-            <?php foreach ($rows as $row): ?>
-                <div class="col-md-2 col-sm-4 col-xs-6">
-                    <div class="card h-100">
-                        <img src="<?= $row["image"] ?>" class="card-img-top" alt="Portada del libro" style="max-width: 100%;">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <?= $row["title"] ?>
-                            </h5>
-                            <p class="card-text">Autor:
-                                <?= $row["author"] ?>
-                            </p>
-                            <p class="card-text">ISBN:
-                                <?= $row["isbn"] ?>
-                            </p>
-                            <a href="view/books/show.php?id=<?= $row[0] ?>" class="btn btn-primary">Ver detalle</a>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    <?php else: ?>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="text-center">Lo sentimos, no se han encontrado libros registrados.</p>
-            </div>
-        </div>
-    <?php endif; ?>
-</div>
+    <a href="/4Library/Biblioteca/view/books/create.php" class="btn btn-primary">Agregar un nuevo libro</a>
 
 <?php
-require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/footer.php");
+    require_once("c://xampp/htdocs/4Library/Biblioteca/view/head/footer.php");
 ?>
--- Biblioteca/view/head/head.php
+++ Biblioteca/view/head/head.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,30 +10,27 @@
         : ((strpos($_SERVER['REQUEST_URI'],'show')) ? "Detalles del registro ".$_GET['id'] : "Actualizar registro ".$_GET['id'] );
     ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="resources/css/style.css">
+    
+    <style>
+        .header-bg {
+            background-image: url('../../resources/img/portada.jpg');
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
   </head>
+  
   <body>
-    <div class="container-fluid bg-dark p-2 mb-3">
+    <div class="header-bg container-fluid p-2 mb-3">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/4Library/Biblioteca/index.php">Inicio</a>
-                
+            <a class="navbar-brand" href="/4Library/Biblioteca/index.php">
+            <img src="4Library/Biblioteca/resources/img/Logo-def.png" alt="Logo" width="100" height="40">
+        </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Books
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/4Library/Biblioteca/view/books/index.php">Libros</a></li>
-
-                                <li><a class="dropdown-item" href="/4Library/Biblioteca/view/books/create.php">Agregar nuevos libros</a></li>
-                            </ul>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </nav>
